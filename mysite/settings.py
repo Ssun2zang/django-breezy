de4518ffe7ae5d82765742a35f7993c92b7d3ca2
Ssun2zang <mmmmhj@cau.ac.kr>
2022-11-05 01:03:18 +0900

    allowed host update

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-@g-m%id_r-k6o0reasi3ui4$^$c6ix2r_21(s8z=+37==fba)(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".ap-northeast-2.compute.amazonaws.com"
+]
 
 
 # Application definition
